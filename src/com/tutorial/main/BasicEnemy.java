1eb45a800d6812ed6d2b583ced321ba67ba7518d
Viktor <vhasselstrm@gmail.com>
2020-03-25 13:56:12 +0100

    Added some directionchanging when enemies hit walls

--- src/com/tutorial/main/BasicEnemy.java
+++ src/com/tutorial/main/BasicEnemy.java
@@ -3,18 +3,21 @@ package com.tutorial.main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.Random;
 
-import org.omg.PortableServer.ThreadPolicyOperations;
 
 public class BasicEnemy extends GameObject{
 	
 	private Handler handler;
 	private Color color;
+	private Random random;
+	private int direction;
 
 	public BasicEnemy(int x, int y, ID id, Handler handler, Color color) {
 		super(x, y, id);
 		this.handler = handler;
 		this.color = color;
+		random = new Random();
 		
 		velX = 3;
 		velY = 3;
@@ -24,12 +27,14 @@ public class BasicEnemy extends GameObject{
 	public void tick() {
 		x += velX;
 		y += velY;
-		
+		direction = random.nextInt(4);
 		if(y <= 0 || y >= Game.HEIGHT - 42) {
 			velY *= -1;
+			velX += direction;
 		}
 		if (x <= 0 || x >= Game.WIDTH - 20) {
 			velX *= -1;
+			velY += direction;
 		}
 		
 		handler.addObject(new Trail(x, y, ID.Trail, color, 16, 16, 0.05f, handler));
